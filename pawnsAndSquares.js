75d19579923acd6671cc647fd5055db833aafcfd
benpapac <ben.papac@gmail.com>
2021-08-26 16:41:08 -0700

    Created script and pawnNames Array

---
+++ pawnsAndSquares.js
@@ -0,0 +1,22 @@
+const pawnNamesArray = [
+	'pink-pawn-A',
+	'pink-pawn-B',
+	'pink-pawn-C',
+	'pink-pawn-D',
+	'pink-pawn-E',
+	'blue-pawn-A',
+	'blue-pawn-B',
+	'blue-pawn-C',
+	'blue-pawn-D',
+	'blue-pawn-E',
+];
+
+
+
+for (let i = 0; i < pawnNamesArray.length; i++) {
+    for (let j = 0; j < columnArray.length; j++) {
+        const element = array[j];
+        
+    }
+    
+}
