7c9b5234073c6bb04467086d2f0b8705ed0e6472
benpapac <ben.papac@gmail.com>
2021-08-27 18:50:15 -0700

    Resolved newGame() bugs.

--- eventHandlers.js
+++ eventHandlers.js
@@ -11,7 +11,6 @@ function handlePick(event) {
 	// if it's not a pawn, or if it's my opponent's pawn, I can't pick it.
 	if (!pawnNamesArray.includes(`${event.target.id}`)) return;
 	if (event.target.classList.contains(`${opponent.class}`)) return;
-
 	updateMovementStates(event);
 }
 
--- script.js
+++ script.js
@@ -9,12 +9,27 @@ const player2 = {
 	class: 'pink-pawn',
 	number: -1,
 };
+
+const rowsArray = ['one', 'two', 'three', 'four', 'five'];
+const columnsArray = ['A', 'B', 'C', 'D', 'E'];
+
+const pawnNamesArray = [
+	'pink-pawn-A',
+	'pink-pawn-B',
+	'pink-pawn-Sage',
+	'pink-pawn-D',
+	'pink-pawn-E',
+	'blue-pawn-A',
+	'blue-pawn-B',
+	'blue-pawn-Sage',
+	'blue-pawn-D',
+	'blue-pawn-E',
+];
 //cached DOM references
 
 const board = document.querySelector(`#board`);
 const message = document.querySelector('#message');
-//message = the place where I'll display player messages
-//resetButton = the button that appears to reset the game.
+const resetButton = document.querySelector('#reset-button');
 
 //State Variables
 let winner;
@@ -23,12 +38,13 @@ let turn;
 let currentPlayer;
 let opponent;
 
-let movementCard;
+let movementCard; //not currently in use.
 let canMove;
 let canAttack;
 let pickMode;
 let activePawn;
 let targetPawn;
+
 let newSquare;
 let oldSquare;
 let oldRow;
@@ -36,34 +52,49 @@ let oldColumn;
 
 //start the game!
 startGame();
+makePawns();
 // EVENT LISTENER //
 board.addEventListener('click', handleClick);
+resetButton.addEventListener('click', startGame);
 
-// FUNCTIONS //
+// CORE FUNCTIONS //
 
 function startGame() {
 	//update State Variables and render()
-	winner = false;
+
+    winner = false;
+    gameOver = false;
 	turn = 1;
 	currentPlayer = player1;
 	opponent = player2;
+    
 	canMove = false;
+    canAttack= false;
 	pickMode = true;
+    activePawn = null;
+    targetPawn = null;
+    
 	newSquare = null;
 	oldSquare = null;
-	// render();
+    oldRow = null;
+    oldColumn = null;
+	render();
 }
 
 function render() {
 	//update the DOM
 
+    if(!activePawn) return;
+    oldSquare.removeChild(activePawn);
+    newSquare.appendChild(activePawn);
 	//  `Player (is winner greater than 0? print 1 otherwise, print 2)  wins!`,
-	if (gameOver)
+	if (gameOver) {
+        deletePawns();
+        makePawns();
 		message.textContent = `Player ${winner.number > 0 ? 1 : 2} wins!`;
-	else message.textContent = `Player ${opponent.number > 0 ? 1 : 2}'s turn.`;
+		resetButton.style.display = 'block';
+	} else message.textContent = `Player ${opponent.number > 0 ? 1 : 2}'s turn.`;
 
-	oldSquare.removeChild(activePawn);
-	newSquare.appendChild(activePawn);
 }
 function newTurn() {
 	resetMovementStates();
@@ -77,6 +108,35 @@ function newTurn() {
 		opponent = player1;
 	}
 }
+function getWinner(targetPawn) {
+    // if either sage is gone, end game.
+    if (targetPawn.id === pawnsList.pinkPawnSage.id) {
+        winner = player1;
+        return (gameOver = true);
+    }
+
+    if (targetPawn.id === pawnsList.bluePawnSage.id) {
+        winner = player2;
+        return (gameOver = true);
+    }
+
+    if (
+        activePawn.id === pawnsList.bluePawnSage.id &&
+        newSquare.id === 'pink-temple'
+    ) {
+        winner = player1;
+        return (gameOver = true);
+    }
+
+    if (
+        activePawn.id === pawnsList.pinkPawnSage.id &&
+        newSquare.id === 'blue-temple'
+    ) {
+        winner = player2;
+        return (gameOver = true);
+    }
+    return (winner = null);
+}
 
 // ALL CALLBACK HANDLERS ON eventHandlers.js //
 function handleClick(event) {
@@ -88,32 +148,39 @@ function handleClick(event) {
 	// newTurn();
 }
 
-function getWinner(targetPawn) {
-	// if either sage is gone, end game.
-	if (targetPawn.id === pawnsList.pinkPawnSage.id) {
-		winner = player1;
-		return (gameOver = true);
-	}
+function deletePawns() {
+    let deadArray= [];
+	for (let i = 0; i < pawnNamesArray.length; i++) {
+		let deadPawn = document.querySelector(`#${pawnNamesArray[i]}`);
+        deadArray.push(deadPawn);
+    }
+    deadArray.forEach(element => {
+        ghostPawn = element;
+        if (ghostPawn === null) return;
+        ghostPawn.remove();
+});
+}
 
-	if (targetPawn.id === pawnsList.bluePawnSage.id) {
-		winner = player2;
-		return (gameOver = true);
-	}
+function makePawns() {
+	for (let column = 0; column < columnsArray.length; column++) {
+		let pinkSquare = document.querySelector(
+			`[data-row= ${rowsArray[4]}] [data-column= ${columnsArray[column]}]`
+		);
+		let blueSquare = document.querySelector(
+			`[data-row= ${rowsArray[0]}] [data-column= ${columnsArray[column]}]`
+		);
 
-	if (
-		activePawn.id === pawnsList.bluePawnSage.id &&
-		newSquare.id === 'pink-temple'
-	) {
-		winner = player1;
-		return (gameOver = true);
-	}
+		let pinkPawn = document.createElement('a');
+		pinkPawn.classList.add('pink-pawn');
+		pinkPawn.classList.add('pawn');
+		pinkPawn.id = pawnNamesArray[column];
 
-	if (
-		activePawn.id === pawnsList.pinkPawnSage.id &&
-		newSquare.id === 'blue-temple'
-	) {
-		winner = player2;
-		return (gameOver = true);
+		let bluePawn = document.createElement('a');
+		bluePawn.classList.add('blue-pawn');
+		bluePawn.classList.add('pawn');
+		bluePawn.id = pawnNamesArray[column + 5];
+
+		pinkSquare.appendChild(pinkPawn);
+		blueSquare.appendChild(bluePawn);
 	}
-	return (winner = null);
 }
