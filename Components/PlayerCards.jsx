5b4d60978e355c090f4a3dcf0dc4f947fb90115e
benpapac <ben.papac@gmail.com>
2022-10-08 11:24:11 -0700

    add pressables to PlayerCards.jsx

--- Components/PlayerCards.jsx
+++ Components/PlayerCards.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text, Pressable } from 'react-native';
 import { Context } from '../Utils/context';
 import { images } from '../Utils/cards';
 import {CardPanelStyles} from '../StyleSheets/CardPanels.js';
@@ -8,24 +8,30 @@ import mantis from '../assets/praying-mantis.png'
 const playerCards = ({player, color}) => {
     let cardOne = images[player[0]];
     let cardTwo = images[player[1]];
+    const [chosenCard, setChosenCard] = useState('');
 
-    console.log(cardOne, cardTwo);
-
-    const handleClick = (e) => {
-
-    }
+    useEffect(()=>{
+        console.log(chosenCard);
+    }, [chosenCard]);
 
     return (
         <View style={CardPanelStyles.panel}>
             <Text style={CardPanelStyles.title}>{color}</Text>
-                <Image nativeID={player[0]} 
-                style={CardPanelStyles.image} 
-                source={{uri: cardOne}}
-                />
+                 
+                <Pressable  nativeID={player[0]} style={{...CardPanelStyles.image, backgroundImage: cardOne}} onPress={() => setChosenCard(player[0])} >
+                        <Image title={player[0]} 
+                    style={CardPanelStyles.image} 
+                    source={{uri: cardOne}}
+                  />
+                </Pressable>
 
-                <Image nativeID={player[1]} 
-                style={CardPanelStyles.image} 
-                source={{uri: cardTwo}}/>
+                <Pressable  nativeID={player[1]} style={CardPanelStyles.image} onPress={() => setChosenCard(player[1])} >
+                    <Image title={player[1]} 
+                    style={CardPanelStyles.image} 
+                    source={{uri: cardTwo}}
+                />
+                </Pressable>
+
                 </View>
     );
 };
